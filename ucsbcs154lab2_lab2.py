b58b6447d537b8ca9f6e990a5c2e7dca37895d70
riasingh455 <riasingh@ucsb.edu>
2025-04-15 14:44:01 -0700

    again2

--- ucsbcs154lab2_lab2.py
+++ ucsbcs154lab2_lab2.py
@@ -21,23 +21,23 @@ alu_out = pyrtl.WireVector(bitwidth=32, name='alu_result')
 
 with pyrtl.conditional_assignment:
     with funct == 32:
-        alu_result |= rs_val + rt_val #add
+        alu_out |= rs_val + rt_val #add
     with funct == 34:
-        alu_result |= rs_val - rt_val #sub
+        alu_out |= rs_val - rt_val #sub
     with funct == 36:
-        alu_result |= rs_val & rt_val #and
+        alu_out |= rs_val & rt_val #and
     with funct == 37:
-        alu_result |= rs_val | rt_val #or
+        alu_out |= rs_val | rt_val #or
     with funct == 38:
-        alu_result |= rs_val ^ rt_val #xor
+        alu_out |= rs_val ^ rt_val #xor
     with funct == 0:
-        alu_result |= pyrtl.shift_left_logical(rt_val, shamt) #sll
+        alu_out |= pyrtl.shift_left_logical(rt_val, shamt) #sll
     with funct == 2:
-        alu_result |= pyrtl.shift_right_logical(rt_val, shamt) #srl
+        alu_out |= pyrtl.shift_right_logical(rt_val, shamt) #srl
     with funct == 3:
-        alu_result |= pyrtl.shift_right_arithmetic(rt_val, shamt)#sra
+        alu_out |= pyrtl.shift_right_arithmetic(rt_val, shamt)#sra
     with funct == 42:
-        alu_result |= pyrtl.signed_lt(rs_val, rt_val) #slt
+        alu_out |= pyrtl.signed_lt(rs_val, rt_val) #slt
 
 #writeback
 rf[rd] <<= alu_out
